b215444b7e630e1bc2409feff78bc468e59f6cb0
Hac_pop_nicard <benkheroufnacim@gmail.com>
2023-12-01 22:54:23 +0100

    first commit

--- tempCodeRunnerFile.dart
+++
@@ -1,9 +0,0 @@
-  // while (i <= j) {
-  //   bool found1 = c.contains(line[i]);
-  //   if (found1) {
-  //     result += line[i];
-  //     break;
-  //   } else {
-  //     i++;
-  //   }
-  // }
